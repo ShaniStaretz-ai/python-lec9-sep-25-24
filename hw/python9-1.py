82ee03e8f6bf44cd4318d047fe01653cd9f60ec9
Shani Staretz <shakimi88@gmail.com>
2024-09-26 20:39:48 +0300

    fixed bugs

--- hw/python9-1.py
+++ hw/python9-1.py
@@ -11,22 +11,27 @@ while True:
 temperatures.extend([-20, 39.1, 18.5])
 newList: list[int] = [4, 5, 6] + [1, 2, 3];  # creates a new list with both lists
 [4, 5, 6].extend([1, 2, 3])  # changes the list [4,5,6] itself
-print("max:", max(temperatures));
-print("min:", min(temperatures));
+if temperatures:
+    print("max:", max(temperatures));
+if temperatures:
+    print("min:", min(temperatures));
 print(f"is 18.5 in list temperatures:{18.5 in temperatures}");
 count_temp_minus_20: int = temperatures.count(-20);
-print(f"avg temperatures:{sum(temperatures) / len(temperatures):.2f}");
+if temperatures:
+    print(f"avg temperatures:{sum(temperatures) / len(temperatures):.2f}");
 print("temperature:", end=' ');
 for temperature in temperatures:
     print(temperature, end=' ');
 print()
 if 39.1 in temperatures:
     index_39_1: int = temperatures.index(39.1);
-del temperatures[0];
-del temperatures[::2];
+if temperatures:
+    del temperatures[0];
+    del temperatures[::2];
 if 18.5 in temperatures:  # remove function will give an error
     temperatures.remove(18.5);
-temp_last: float = temperatures.pop()
+if temperatures:
+    temp_last: float = temperatures.pop()
 new_temperatures: list[float] = temperatures.copy();
 new_temperatures.sort();
 new_temperatures2: list[float] = temperatures.copy();
