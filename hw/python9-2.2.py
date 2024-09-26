8b2d6d5dfeb921759c67f9afa8e46fe9a1b99629
Shani Staretz <shakimi88@gmail.com>
2024-09-26 14:57:40 +0300

    after CR

--- hw/python9-2.2.py
+++ hw/python9-2.2.py
@@ -7,11 +7,11 @@ while True:
     number: int = int(input("enter temperature:"))
     if number == SENTINEL:
         break;
-    if number > 10 or number < 0:
+    if number > 100 or number < 0:
         continue;
     count_numbers[number - 1] += 1
 
-str_results = ''
+str_results:str = ''
 for i in range(len(count_numbers)):
     str_results += f'[{i + 1}]:{count_numbers[i]} '
 print('Statistics:' + str_results)
