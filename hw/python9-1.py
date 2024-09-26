4068a2d7d8a9b5103a8e7fbda788ade30a637af2
Shani Staretz <shakimi88@gmail.com>
2024-09-26 11:58:35 +0300

    hw1

---
+++ hw/python9-1.py
@@ -0,0 +1,32 @@
+temperatures: list[float] = [];
+SENTINEL: int = -999
+while True:
+    temp: float = float(input("enter temperature:"))
+    if temp == SENTINEL:
+        break;
+    if temp > 50 or temp < -50:
+        continue;
+    temperatures.append(temp)
+
+temperatures.extend([-20, 39.1, 18.5])
+newList: list[int] = [4, 5, 6] + [1, 2, 3];  # creates a new list
+[4, 5, 6].extend([1, 2, 3])  # changes the list [4,5,6]
+print("max:", max(temperatures))
+print("min:", min(temperatures))
+print(f"is 18.5 in list temperatures:{18.5 in temperatures}")
+count_temp_minus_20: int = temperatures.count(-20)
+print(f"avg temperatures:{sum(temperatures) / len(temperatures):.2f}")
+for temperature in temperatures:
+    print(temperature)
+if 39.1 in temperatures:
+    index_39_1: int = temperatures.index(39.1)
+del temperatures[0];
+del temperatures[::2]
+if 18.5 in temperatures:  # remove function will give an error
+    temperatures.remove(18.5)
+temp_last: float = temperatures.pop()
+new_temperatures: list[float] = temperatures.copy();
+new_temperatures.sort()
+
+new_temperatures2: list[float] = temperatures.copy();
+new_temperatures2.sort(reverse=True)
