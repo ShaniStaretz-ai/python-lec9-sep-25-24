34146f9af594ad16f3104c9be743a217ff4e422f
Shani Staretz <shakimi88@gmail.com>
2024-09-29 17:03:13 +0300

    fixed bugs

--- hw/python9-2.2.py
+++ hw/python9-2.2.py
@@ -4,14 +4,15 @@ SENTINEL: int = -999;
 for _ in range(100):
     count_numbers.append(0)
 while True:
-    number: int = int(input("enter temperature:"))
+    number: int = int(input("enter number:"))
     if number == SENTINEL:
         break;
     if number > 100 or number < 0:
         continue;
     count_numbers[number - 1] += 1
 
-str_results:str = ''
+str_results: str = ''
 for i in range(len(count_numbers)):
-    str_results += f'[{i + 1}]:{count_numbers[i]} '
+    if count_numbers[i]:
+        str_results += f'[{i + 1}]:{count_numbers[i]} '
 print('Statistics:' + str_results)
